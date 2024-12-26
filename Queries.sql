ab632929a0cfed1ea74672d657bc298c055f8fff
Nguyen Van Linh <23020395@vnu.edu.vn>
2024-12-26 13:57:11 +0700

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -21,12 +21,14 @@ WHERE PhieuThue.GiaThue > (SELECT AVG(GiaThue) FROM PhieuThue);
 
 -- 4. Using Subquery in FROM
 -- Lấy danh sách khách hàng có tổng tiền thuê lớn hơn 100 triệu
-SELECT TenKH, TongTien
-FROM (SELECT KhachHang.HoTen AS TenKH, SUM(PhieuThue.GiaThue) AS TongTien
-      FROM KhachHang
-      INNER JOIN PhieuThue ON KhachHang.MaKH = PhieuThue.MaKH
-      GROUP BY KhachHang.MaKH) AS Subquery
-WHERE TongTien > 10000000;
+SELECT KH.MaKH, KH.HoTen, KH.SoDienThoai, KH.CoQuan, TotalRent.TongTienThue
+FROM (
+    SELECT PT.MaKH, SUM(PT.GiaThue * (DATEDIFF(PT.NgayKetThuc, PT.NgayBatDau)/30)) AS TongTienThue
+    FROM PhieuThue PT
+    GROUP BY PT.MaKH
+) AS TotalRent
+JOIN KhachHang KH ON KH.MaKH = TotalRent.MaKH
+WHERE TotalRent.TongTienThue > 100000000;
 
 -- 5. Query using GROUP BY and Aggregate Functions
 -- Tính tổng số tiền thuê của các khách hàng
@@ -34,3 +36,5 @@ SELECT KhachHang.HoTen AS TenKH, SUM(PhieuThue.GiaThue) AS TongTienThue
 FROM KhachHang
 INNER JOIN PhieuThue ON KhachHang.MaKH = PhieuThue.MaKH
 GROUP BY KhachHang.MaKH;
+
+
